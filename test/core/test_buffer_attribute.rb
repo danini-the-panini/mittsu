0a3a1fda50ff182043e241f3836a33ea90edf5be
Daniel Smith <jellymann@gmail.com>
2015-07-14 00:07:10 +0200

    test: buffer attribute methods return self

--- test/core/test_buffer_attribute.rb
+++ test/core/test_buffer_attribute.rb
@@ -11,65 +11,73 @@ class TestBufferAttribute < Minitest::Test
     a = Mittsu::BufferAttribute.new([0, 0, 0, 0, 0], 2)
     b = Mittsu::BufferAttribute.new([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)
 
-    a.copy_at 1, b, 2
+    result = a.copy_at 1, b, 2
 
     assert_equal [0, 0, 7, 8, 0], a.array
+    assert_same a, result
   end
 
   def test_set
     a = Mittsu::BufferAttribute.new([0, 0, 0, 0, 0, 0, 0, 0], 3)
 
-    a.set([1, 2, 3], 2)
+    result = a.set([1, 2, 3], 2)
 
     assert_equal [0, 0, 1, 2, 3, 0, 0, 0], a.array
+    assert_same a, result
   end
 
   def test_set_x
     a = Mittsu::BufferAttribute.new([0, 0, 0, 0, 0, 0, 0, 0, 0], 3)
 
-    a.set_x 1, 42
+    result = a.set_x 1, 42
 
     assert_equal [0, 0, 0, 42, 0, 0, 0, 0, 0], a.array
+    assert_same a, result
   end
 
   def test_set_y
     a = Mittsu::BufferAttribute.new([0, 0, 0, 0, 0, 0, 0, 0, 0], 3)
 
-    a.set_y 1, 42
+    result = a.set_y 1, 42
 
     assert_equal [0, 0, 0, 0, 42, 0, 0, 0, 0], a.array
+    assert_same a, result
   end
 
   def test_set_z
     a = Mittsu::BufferAttribute.new([0, 0, 0, 0, 0, 0, 0, 0, 0], 3)
 
-    a.set_z 1, 42
+    result = a.set_z 1, 42
 
     assert_equal [0, 0, 0, 0, 0, 42, 0, 0, 0], a.array
+    assert_same a, result
   end
 
   def test_set_xy
     a = Mittsu::BufferAttribute.new([0, 0, 0, 0, 0, 0, 0, 0, 0], 3)
 
-    a.set_xy 1, 42, 24
+    result = a.set_xy 1, 42, 24
 
     assert_equal [0, 0, 0, 42, 24, 0, 0, 0, 0], a.array
+    assert_same a, result
   end
 
   def test_set_xyz
     a = Mittsu::BufferAttribute.new([0, 0, 0, 0, 0, 0, 0, 0, 0], 3)
 
-    a.set_xyz 1, 42, 24, 12
+    result = a.set_xyz 1, 42, 24, 12
 
     assert_equal [0, 0, 0, 42, 24, 12, 0, 0, 0], a.array
+    assert_same a, result
   end
 
   def test_set_xyzw
     a = Mittsu::BufferAttribute.new([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 4)
 
-    a.set_xyzw 1, 42, 24, 12, 6
+    result = a.set_xyzw 1, 42, 24, 12, 6
 
     assert_equal [0, 0, 0, 0, 42, 24, 12, 6, 0, 0, 0, 0], a.array
+    assert_same a, result
   end
 
   def test_clone
