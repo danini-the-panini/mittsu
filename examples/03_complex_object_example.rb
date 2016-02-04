c85c7773e8411fc0f9ffc899fac4038d8cee6d9f
Daniel Smith <jellymann@gmail.com>
2016-02-04 17:35:36 +0200

    feat: use print_tree in complex object example

--- examples/03_complex_object_example.rb
+++ examples/03_complex_object_example.rb
@@ -5,25 +5,34 @@ SCREEN_HEIGHT = 600
 ASPECT = SCREEN_WIDTH.to_f / SCREEN_HEIGHT.to_f
 
 scene = Mittsu::Scene.new
+scene.name = 'Root Scene'
 camera = Mittsu::PerspectiveCamera.new(75.0, ASPECT, 0.1, 1000.0)
 
 renderer = Mittsu::OpenGLRenderer.new width: SCREEN_WIDTH, height: SCREEN_HEIGHT, title: '03 Complex Object Example'
 
-sphere_geometry = Mittsu::SphereGeometry.new(1.0)
-box_geometry = Mittsu::BoxGeometry.new(1.0, 1.0, 1.0)
-green_material = Mittsu::MeshBasicMaterial.new(color: 0x00ff00)
-blue_material = Mittsu::MeshBasicMaterial.new(color: 0x0000ff)
-cube = Mittsu::Mesh.new(box_geometry, green_material)
-sphere = Mittsu::Mesh.new(sphere_geometry, blue_material)
+objects = 3.times.map do |i|
+  sphere_geometry = Mittsu::SphereGeometry.new(1.0)
+  box_geometry = Mittsu::BoxGeometry.new(1.0, 1.0, 1.0)
+  green_material = Mittsu::MeshBasicMaterial.new(color: 0x00ff00)
+  blue_material = Mittsu::MeshBasicMaterial.new(color: 0x0000ff)
+  cube = Mittsu::Mesh.new(box_geometry, green_material)
+  cube.name = 'Green Cube'
+  sphere = Mittsu::Mesh.new(sphere_geometry, blue_material)
+  sphere.name = 'Blue Ball'
 
-cube.position.set(0.5, 0.0, 0.0)
-sphere.position.set(-0.5, 0.0, 0.0)
+  cube.position.set(0.5, 0.0, 0.0)
+  sphere.position.set(-0.5, 0.0, 0.0)
 
-object = Mittsu::Object3D.new
-object.add(cube)
-object.add(sphere)
+  Mittsu::Object3D.new.tap do |o|
+    o.add(cube)
+    o.add(sphere)
+    o.position.x = -3.0 + (i.to_f * 3.0)
 
-scene.add(object)
+    scene.add(o)
+  end
+end
+
+scene.print_tree
 
 camera.position.z = 5.0
 
@@ -34,8 +43,10 @@ renderer.window.on_resize do |width, height|
 end
 
 renderer.window.run do
-  object.rotation.x += 0.1
-  object.rotation.y += 0.1
+  objects.each do |object|
+    object.rotation.x += 0.1
+    object.rotation.y += 0.1
+  end
 
   renderer.render(scene, camera)
 end
