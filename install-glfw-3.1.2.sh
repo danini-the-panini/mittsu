8a4b2703e15492d727ea7178c0e29deb88743b0b
Daniel Smith <jellymann@gmail.com>
2016-02-18 10:47:13 +0200

    fix: cd into glfw dir after unzip in install script

--- install-glfw-3.1.2.sh
+++ install-glfw-3.1.2.sh
@@ -3,6 +3,7 @@ set -e
 if [ ! -e glfw3-3.1.2/src/libglfw3.a ]; then
   wget https://github.com/glfw/glfw/releases/download/3.1.2/glfw-3.1.2.zip
   unzip glfw-3.1.2.zip
+  cd glfw-3.1.2
   cmake .
   make && sudo make install;
 fi
