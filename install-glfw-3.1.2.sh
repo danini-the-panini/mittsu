a7ee614c9a36c37d74ffc529ac602c5a822926fb
Daniel Smith <jellymann@gmail.com>
2016-02-21 14:35:36 +0200

    patch: update install glfw script only unzip when necessary

--- install-glfw-3.1.2.sh
+++ install-glfw-3.1.2.sh
@@ -1,8 +1,10 @@
 set -x
 set -e
-if [ ! -e glfw3-3.1.2/src/libglfw3.a ]; then
+if [ ! -e glfw-3.1.2/include/GLFW/glfw3.h ]; then
   wget https://github.com/glfw/glfw/releases/download/3.1.2/glfw-3.1.2.zip
-  unzip glfw-3.1.2.zip
+  unzip glfw-3.1.2.zip;
+fi
+if [ ! -e glfw3-3.1.2/src/libglfw3.a ]; then
   cd glfw-3.1.2
   cmake .
   make && sudo make install;
