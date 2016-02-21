12cbbcc8386dcca892afae0d83808487b013ae91
Daniel Smith <jellymann@gmail.com>
2016-02-21 16:13:39 +0200

    feat: JUnit minitest reports for CI

--- mittsu.gemspec
+++ mittsu.gemspec
@@ -26,6 +26,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.9"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency 'minitest', '~> 5.7'
+  spec.add_development_dependency 'minitest-reporters', '~> 1.1'
   spec.add_development_dependency 'pry', '~> 0.10'
   spec.add_development_dependency 'benchmark-ips', '~> 2.3'
 end
--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,3 +1,12 @@
+require "minitest/reporters"
+REPORTER = "#{ENV['MINITEST_REPORTER'] || 'Progress'}Reporter"
+if !Minitest::Reporters.const_defined?(REPORTER)
+  puts "WARNING: Reporter \"#{REPORTER}\" not found, using default"
+  Minitest::Reporters.use!
+else
+  Minitest::Reporters.use! Minitest::Reporters.const_get(REPORTER).new
+end
+
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'minitest'
 require 'mittsu'
