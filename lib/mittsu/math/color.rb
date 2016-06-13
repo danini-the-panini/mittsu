ebe8e981626061c22527fb2989b5c96272fc0f27
Daniel Smith <jellymann@gmail.com>
2016-06-13 10:43:42 +0200

    refactor: color class

--- lib/mittsu/math/color.rb
+++ lib/mittsu/math/color.rb
@@ -1,10 +1,13 @@
 require 'mittsu/math'
+require 'mittsu/math/vector3'
 
 module Mittsu
-  class Color
-    attr_accessor :r, :g, :b
+  class Color < Vector3
+    ELEMENTS = { r: 0, g: 1, b: 2 }
+    DIMENSIONS = ELEMENTS.count
 
     def initialize(*args)
+      super(0, 0, 0)
       case args.length
       when 3 then self.set_rgb(*args)
       when 1 then self.set(args.first)
@@ -12,6 +15,14 @@ module Mittsu
       else raise ArgumentError, "Arguments must be (r, g, b), (color), or none"
       end
     end
+
+    alias :r :x
+    alias :g :y
+    alias :b :z
+
+    alias :r= :x=
+    alias :g= :y=
+    alias :b= :z=
 
     def set(value)
       case value
@@ -29,29 +40,27 @@ module Mittsu
 
     def set_hex(hex)
       hex = hex.floor
-      @r = (hex >> 16 & 255) / 255.0
-      @g = (hex >> 8 & 255) / 255.0
-      @b = (hex & 255) / 255.0
+      self.r = (hex >> 16 & 255) / 255.0
+      self.g = (hex >> 8 & 255) / 255.0
+      self.b = (hex & 255) / 255.0
       self
     end
 
     def set_rgb(r, g, b)
-      @r = r.to_f
-      @g = g.to_f
-      @b = b.to_f
+      @elements = [r.to_f, g.to_f, b.to_f]
       self
     end
 
     def set_hsl(h, s, l)
       # h,s,l ranges are in 0.0 - 1.0
       if s.zero?
-        @r = @g = @b = l
+        self.r = self.g = self.b = l
       else
         p = l <= 0.5 ? l * (1.0 + s) : l + s - (l * s)
         q = (2.0 * l) - p
-        @r = hue2rgb(q, p, h + 1.0 / 3.0)
-        @g = hue2rgb(q, p, h)
-        @b = hue2rgb(q, p, h - 1.0 / 3.0)
+        self.r = hue2rgb(q, p, h + 1.0 / 3.0)
+        self.g = hue2rgb(q, p, h)
+        self.b = hue2rgb(q, p, h - 1.0 / 3.0)
       end
       self
     end
@@ -59,16 +68,16 @@ module Mittsu
     def set_style(style)
       # rgb(255,0,0)
       if /^rgb\((\d+), ?(\d+), ?(\d+)\)$/i =~ style
-        @r = [255.0, $1.to_f].min / 255.0
-        @g = [255.0, $2.to_f].min / 255.0
-        @b = [255.0, $3.to_f].min / 255.0
+        self.r = [255.0, $1.to_f].min / 255.0
+        self.g = [255.0, $2.to_f].min / 255.0
+        self.b = [255.0, $3.to_f].min / 255.0
         return self
       end
       # rgb(100%,0%,0%)
       if /^rgb\((\d+)\%, ?(\d+)\%, ?(\d+)\%\)$/i =~ style
-        @r = [100.0, $1.to_f].min / 100.0
-        @g = [100.0, $2.to_f].min / 100.0
-        @b = [100.0, $3.to_f].min / 100.0
+        self.r = [100.0, $1.to_f].min / 100.0
+        self.g = [100.0, $2.to_f].min / 100.0
+        self.b = [100.0, $3.to_f].min / 100.0
         return self
       end
       # #ff0000
@@ -88,45 +97,38 @@ module Mittsu
       end
     end
 
-    def copy(color)
-      @r = color.r
-      @g = color.g
-      @b = color.b
-      self
-    end
-
     def copy_gamma_to_linear(color, gamma_factor = 2.0)
-      @r = color.r ** gamma_factor
-      @g = color.g ** gamma_factor
-      @b = color.b ** gamma_factor
+      self.r = color.r ** gamma_factor
+      self.g = color.g ** gamma_factor
+      self.b = color.b ** gamma_factor
       self
     end
 
     def copy_linear_to_gamma(color, gamma_factor = 2.0)
       safe_inverse = (gamma_factor > 0) ? (1.0 / gamma_factor) : 1.0
-      @r = color.r ** safe_inverse
-      @g = color.g ** safe_inverse
-      @b = color.b ** safe_inverse
+      self.r = color.r ** safe_inverse
+      self.g = color.g ** safe_inverse
+      self.b = color.b ** safe_inverse
       self
     end
 
     def convert_gamma_to_linear
-      rr, gg, bb = @r, @g, @b
-      @r = rr * rr
-      @g = gg * gg
-      @b = bb * bb
+      rr, gg, bb = self.r, self.g, self.b
+      self.r = rr * rr
+      self.g = gg * gg
+      self.b = bb * bb
       self
     end
 
     def convert_linear_to_gamma
-      @r = Math.sqrt(@r)
-      @g = Math.sqrt(@g)
-      @b = Math.sqrt(@b)
+      self.r = Math.sqrt(self.r)
+      self.g = Math.sqrt(self.g)
+      self.b = Math.sqrt(self.b)
       self
     end
 
     def hex
-      (@r * 255).to_i << 16 ^ (@g * 255).to_i << 8 ^ (@b * 255).to_i << 0
+      (self.r * 255).to_i << 16 ^ (self.g * 255).to_i << 8 ^ (self.b * 255).to_i << 0
     end
 
     def hex_string
@@ -136,7 +138,7 @@ module Mittsu
     def hsl(target = nil)
       # h,s,l ranges are in 0.0 - 1.0
       hsl = target || { h: 0.0, s: 0.0, l: 0.0 }
-      rr, gg, bb = @r, @g, @b
+      rr, gg, bb = self.r, self.g, self.b
       max = [r, g, b].max
       min = [r, g, b].min
       hue, saturation = nil, nil
@@ -161,7 +163,7 @@ module Mittsu
     end
 
     def style
-      "rgb(#{ (@r * 255).to_i },#{ (@g * 255).to_i },#{ (@b * 255).to_i })"
+      "rgb(#{ (self.r * 255).to_i },#{ (self.g * 255).to_i },#{ (self.b * 255).to_i })"
     end
 
     def offset_hsl(h, s, l)
@@ -173,84 +175,7 @@ module Mittsu
       self
     end
 
-    def add(color)
-      @r += color.r
-      @g += color.g
-      @b += color.b
-      self
-    end
-
-    def add_colors(color1, color2)
-      @r = color1.r + color2.r
-      @g = color1.g + color2.g
-      @b = color1.b + color2.b
-      self
-    end
-
-    def add_scalar(s)
-      @r += s
-      @g += s
-      @b += s
-      self
-    end
-
-    def multiply(color)
-      @r *= color.r
-      @g *= color.g
-      @b *= color.b
-      self
-    end
-
-    def multiply_scalar(s)
-      @r *= s
-      @g *= s
-      @b *= s
-      self
-    end
-
-    def lerp(color, alpha)
-      @r += (color.r - @r) * alpha
-      @g += (color.g - @g) * alpha
-      @b += (color.b - @b) * alpha
-      self
-    end
-
-    def ==(c)
-      (c.r == @r) && (c.g == @g) && (c.b == @b)
-    end
-
-    def []=(index, value)
-      return @r = value.to_f if index == 0 || index == :r
-      return @g = value.to_f if index == 1 || index == :g
-      return @b = value.to_f if index == 2 || index == :b
-      raise IndexError
-    end
-
-    def [](index)
-      return @r if index == 0 || index == :r
-      return @g if index == 1 || index == :g
-      return @b if index == 2 || index == :b
-      raise IndexError
-    end
-
-    def from_array(array)
-      @r = array[0]
-      @g = array[1]
-      @b = array[2]
-      self
-    end
-
-    def to_array(array = [], offset = 0)
-      array[offset] = @r
-      array[offset + 1] = @g
-      array[offset + 2] = @b
-      array
-    end
-    alias :to_a :to_array
-
-    def clone
-      Mittsu::Color.new(@r, @g, @b)
-    end
+    alias :add_colors :add_vectors
 
     private
 
