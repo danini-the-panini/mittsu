e76ebd97c3e355729f7175f48afb34ea7281632d
Daniel Smith <jellymann@gmail.com>
2017-03-27 11:52:11 +0200

    patch: change remove object example title

--- examples/18_remove_object_example.rb
+++ examples/18_remove_object_example.rb
@@ -7,7 +7,7 @@ ASPECT = SCREEN_WIDTH.to_f / SCREEN_HEIGHT.to_f
 scene = Mittsu::Scene.new
 camera = Mittsu::PerspectiveCamera.new(75.0, ASPECT, 0.1, 1000.0)
 
-renderer = Mittsu::OpenGLRenderer.new width: SCREEN_WIDTH, height: SCREEN_HEIGHT, title: '02 Box Mesh Example'
+renderer = Mittsu::OpenGLRenderer.new width: SCREEN_WIDTH, height: SCREEN_HEIGHT, title: '18 Remove Object Example'
 
 cubes = Mittsu::Group.new
 GEOMETRY = Mittsu::BoxGeometry.new(1.0, 1.0, 1.0)
