78baf451e818e8e3954d97e9a31ac8f66ebf80a8
James Smith <james@floppy.org.uk>
2024-02-19 11:41:30 +0000

    add basic STL loader and test

--- lib/mittsu/loaders/stl_loader.rb
+++ lib/mittsu/loaders/stl_loader.rb
@@ -1,9 +1,12 @@
+require 'stringio'
+
 module Mittsu
   class STLLoader
     include EventDispatcher
 
     def initialize(manager = DefaultLoadingManager)
       @manager = manager
+      @vertex_count = 0
       @_listeners = {}
     end
 
@@ -14,10 +17,65 @@ module Mittsu
       parse(text)
     end
 
-    def parse(data)
-      # TODO
+    def parse(text)
+      stream = StringIO.new(text)
+      parse_ascii(stream)
+    end
+
+    private
+
+    def parse_ascii(stream)
+      @group = Group.new
+      while line = stream.gets
+        case line
+        when /^solid/
+          @group.add parse_ascii_solid(stream)
+        end
+      end
       @group
     end
 
+    def parse_ascii_solid(stream)
+      geometry = Geometry.new
+      while line = stream.gets
+        case line
+        when /^\s*facet/
+          vertices, face = parse_ascii_facet(line, stream)
+          geometry.vertices += vertices
+          geometry.faces << face
+        when /^endsolid/
+          break
+        end
+      end
+      geometry.merge_vertices
+      geometry.compute_bounding_sphere
+      object = Object3D.new
+      object.add(Mesh.new(geometry))
+      object
+    end
+
+    def parse_ascii_facet(line, stream)
+      vertices = []
+      normal = nil
+      if line.match /([\d\.]+) ([\d\.]+) ([\d\.]+)/
+        normal = Vector3.new($1, $2, $3)
+      end
+      while line = stream.gets
+        case line
+        when /^\s*outer loop/
+          nil # Ignored
+        when /^\s*endloop/
+          nil # Ignored
+        when /^\s*vertex ([\d\.]+) ([\d\.]+) ([\d\.]+)/
+          vertices << Vector3.new($1, $2, $3)
+        when /^\s*endfacet/
+          break
+        end
+      end
+      return nil if vertices.length != 3
+      face = Face3.new(@vertex_count, @vertex_count+1, @vertex_count+2, normal)
+      @vertex_count += 3
+      return vertices, face
+    end
   end
 end
--- test/loaders/test_stl_loader.rb
+++ test/loaders/test_stl_loader.rb
@@ -36,23 +36,23 @@ endsolid
     [
       Mittsu::Vector3.new(0.0, 2.0, 0.0),
       Mittsu::Vector3.new(0.0, 0.0, 0.0),
-      Mittsu::Vector3.new(2.0, 0.0, 0.0),
-      Mittsu::Vector3.new(2.0, 2.0, 0.0)
+      Mittsu::Vector3.new(2.0, 2.0, 0.0),
+      Mittsu::Vector3.new(2.0, 0.0, 0.0)
     ].each_with_index { |v, i|
       assert_equal v, square_mesh.geometry.vertices[i]
     }
     [
-      [0, 1, 3],
-      [1, 2, 3]
+      [0, 1, 2],
+      [1, 3, 2]
     ].each_with_index { |f, i|
       face = square_mesh.geometry.faces[i]
       a = f[0]
       b = f[1]
       c = f[2]
-
       assert_equal(a, face.a)
       assert_equal(b, face.b)
       assert_equal(c, face.c)
+      assert_equal(Mittsu::Vector3.new(0, 0, 1), face.normal)
     }
   end
 
