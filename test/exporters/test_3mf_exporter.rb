5427b9baddecf775621fa86e040268149135f0d5
James Smith <james@floppy.org.uk>
2024-02-21 12:37:20 +0000

    remove extraneous Object3D and Group in export test

--- test/exporters/test_3mf_exporter.rb
+++ test/exporters/test_3mf_exporter.rb
@@ -6,14 +6,10 @@ class Test3MFExporter < Minitest::Test
     box = Mittsu::Mesh.new(
       Mittsu::BoxGeometry.new(1.0, 1.0, 1.0)
     )
-    obj = Mittsu::Object3D.new
-    obj.add(box)
-    group = Mittsu::Group.new
-    group.add obj
 
     # Export to 3MF
     exporter = Mittsu::ThreeMFExporter.new
-    file = exporter.send(:export_uncompressed, group)
+    file = exporter.send(:export_uncompressed, box)
 
     assert_equal file, 42
   end
