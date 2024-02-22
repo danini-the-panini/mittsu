d3020fe9e5a6cd2fd1559f8d21b56da58ad0e596
James Smith <james@floppy.org.uk>
2024-02-22 15:15:11 +0000

    create 3MF OPC package metadata files

--- lib/mittsu/exporters/3mf_exporter.rb
+++ lib/mittsu/exporters/3mf_exporter.rb
@@ -18,19 +18,49 @@ module Mittsu
     private
 
     def export_uncompressed(dir, object)
-      FileUtils.mkdir_p File.join(dir, "3D")
-      export_3d_model_part(dir, object)
+      export_content_types(dir)
+      filename = export_3d_model_part(dir, object)
+      export_rels(dir, [filename])
+    end
+
+    def export_content_types(dir)
+      filename = File.join(dir, "[Content_Types].xml")
+      File.open(filename, "wb") do |file|
+        xml = Builder::XmlMarkup.new(target: file, indent: 2)
+        xml.instruct! :xml, encoding: "UTF-8"
+        xml.Types xmlns: "http://schemas.openxmlformats.org/package/2006/content-types" do
+          xml.Default Extension: "rels", ContentType: "application/vnd.openxmlformats-package.relationships+xml"
+          xml.Default Extension: "model", ContentType: "application/vnd.ms-package.3dmanufacturing-3dmodel+xml"
+        end
+      end
+    end
+
+    def export_rels(dir, models)
+      FileUtils.mkdir_p File.join(dir, "_rels")
+      filename = File.join(dir, "_rels/.rels")
+      File.open(filename, "wb") do |file|
+        xml = Builder::XmlMarkup.new(target: file, indent: 2)
+        xml.instruct! :xml, encoding: "UTF-8"
+        xml.Relationships xmlns: "http://schemas.openxmlformats.org/package/2006/relationships" do
+          models.each do |name|
+            xml.Relationship Target: "/3D/#{name}.model", Id: name, ContentType: "http://schemas.microsoft.com/3dmanufacturing/2013/01/3dmodel"
+          end
+        end
+      end
     end
 
     def export_3d_model_part(dir, object)
-      filename = File.join(dir, "3D/#{object.name || SecureRandom.uuid}.model")
-      File.open(filename, "wb") do |file|
+      FileUtils.mkdir_p File.join(dir, "3D")
+      filename = object.name.parameterize || SecureRandom.uuid
+      pathname = File.join(dir, "3D/#{filename}.model")
+      File.open(pathname, "wb") do |file|
         xml = Builder::XmlMarkup.new(target: file, indent: 2)
         xml.instruct! :xml, encoding: "UTF-8"
         xml.model unit: "millimeter", "xml:lang": "en-US", xmlns:"http://schemas.microsoft.com/3dmanufacturing/core/2015/02" do
 
         end
       end
+      filename
     end
   end
 end
--- test/exporters/test_3mf_exporter.rb
+++ test/exporters/test_3mf_exporter.rb
@@ -16,8 +16,20 @@ class Test3MFExporter < Minitest::Test
     @exporter.send(:export_uncompressed, @tmpdir, @box)
     REXML::Document.new File.read(File.join(@tmpdir, "3D/box.model"))
     assert_equal "millimeter", REXML::XPath.first(xml, "/model/@unit").value
+
+  def test_3mf_exports_content_types
+    @exporter.send(:export_uncompressed, @tmpdir, @box)
+    xml = REXML::Document.new File.read(File.join(@tmpdir, "[Content_Types].xml"))
+    assert_equal "application/vnd.openxmlformats-package.relationships+xml",
+      REXML::XPath.first(xml, "/Types/Default[@Extension='rels']/@ContentType").value
   end
 
+  def test_3mf_exports_rels
+    @exporter.send(:export_uncompressed, @tmpdir, @box)
+    xml = REXML::Document.new File.read(File.join(@tmpdir, "_rels/.rels"))
+    assert_equal "/3D/box.model",
+      REXML::XPath.first(xml, "/Relationships/Relationship/@Target").value
+  end
 
   def test_export_method_alias
     assert @exporter.parse(Mittsu::Group.new())
