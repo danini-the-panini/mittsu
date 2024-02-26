31b46cfd3a3ab76dbcd9de0ba04628f4a0031356
James Smith <james@floppy.org.uk>
2024-02-26 14:58:12 +0000

    add simple OBJ example file and use it in 3MF conversion example

--- examples/10_3mf_exporter_example.rb
+++ examples/10_3mf_exporter_example.rb
@@ -1,7 +1,7 @@
 require_relative './example_helper'
 
 loader = Mittsu::OBJLoader.new
-object = loader.load(File.expand_path('../male02.obj', __FILE__))
+object = loader.load(File.expand_path('../mittsu.obj', __FILE__))
 
 exporter = Mittsu::ThreeMFExporter.new
-exporter.export(object, File.expand_path('../output.3mf', __FILE__))
+exporter.export(object, File.expand_path('../mittsu-export.3mf', __FILE__))
